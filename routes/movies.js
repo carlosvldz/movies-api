499756c3adfc0d7d2317943a43116e2a763a440d
carlosvldz <me@carlosvldz.com>
2020-10-04 21:08:30 -0500

    implementing Joi

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
+const {
+    movieIdSchema,
+    createMovieSchema,
+    updateMovieSchema
+  } = require('../utils/schemas/movies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
 function moviesApi(app) {
     const router = express.Router();
     app.use("/api/movies", router);
@@ -12,7 +20,6 @@ function moviesApi(app) {
 
         try {
             const movies = await moviesService.getMovies({ tags });
-            throw new Error('Error getting movies');
 
             res.status(200).json({
                 data: movies,
@@ -24,7 +31,7 @@ function moviesApi(app) {
         }
     });
 
-    router.get("/:movieId", async function(req, res, next) {
+    router.get("/:movieId", validationHandler({ movieId: movieIdSchema }, 'params'), async function(req, res, next) {
         const { movieId } = req.params;
 
         try {
@@ -40,7 +47,7 @@ function moviesApi(app) {
         }
     });
 
-    router.post("/", async function(req, res, next) {
+    router.post("/", validationHandler(createMovieSchema), async function(req, res, next) {
         const { body: movie } = req;
         
         try {
@@ -56,7 +63,8 @@ function moviesApi(app) {
         }
     });
 
-    router.put("/:movieId", async function(req, res, next) {
+    router.put("/:movieId", validationHandler({ movieId: movieIdSchema }, 'params'),
+    validationHandler(updateMovieSchema), async function(req, res, next) {
         const { movieId } = req.params;
         const { body: movie } = req;
 
@@ -76,7 +84,7 @@ function moviesApi(app) {
         }
     });
 
-    router.delete("/:movieId", async function(req, res, next) {
+    router.delete("/:movieId", validationHandler({ movieId: movieIdSchema }, 'params'), async function(req, res, next) {
         const { movieId } = req.params;
 
         try {
