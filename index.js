82b4cb612663a07183e4ef40ebc36a49efd2071c
carlosvldz <me@carlosvldz.com>
2020-10-04 20:23:27 -0500

    add Boom

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const { config } = require('./config/index');
 
 const moviesApi = require('./routes/movies')
 
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/noFoundHandler');
+
+// Body parser
 app.use(express.json());
 
+// Routes
 moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
     console.log(`Listening http://localhost:${config.port}`);
